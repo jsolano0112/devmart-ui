6c4e33495270b2d709a5483c2ce3be7fe1e4092c
Juana Solano <juana.solano@correo.tdea.edu.co>
2025-11-08 09:15:07 -0500

    Complete login feature

---
+++ src/api/authService.js
@@ -0,0 +1,26 @@
+import api, { setAuthTokens } from "./apiClient";
+import { AUTH } from "./endpointPaths";
+
+export const loginUser = async (email, password) => {
+  try {
+    const { data } = await api.post(AUTH.login, { email, password });
+
+    const { id, firstname, lastname, isAdmin, accessToken, refreshToken } = data;
+
+    setAuthTokens({ accessToken, refreshToken });
+    localStorage.setItem("tokens", JSON.stringify({ accessToken, refreshToken }));
+
+    return {
+      ok: true,
+      id,
+      firstname,
+      lastname,
+      isAdmin,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      errorMessage: error.response?.data?.errors?.[0]?.msg ?? error.response?.data?.error
+    };
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from './contexts/UserProvider';
+import { AppRouter } from './routers/AppRouter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <AppRouter />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
